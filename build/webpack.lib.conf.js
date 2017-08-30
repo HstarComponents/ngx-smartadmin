829ae905ac1194f655aa87cc7463d83c62a65353
Jay.M.Hu <jay.m.hu@newegg.com>
2017-08-30 17:33:02 +0800

    Add lib build script

---
+++ build/webpack.lib.conf.js
@@ -0,0 +1,47 @@
+require('shelljs/global');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+const baseConfig = require('./webpack.base.conf');
+const util = require('./util');
+
+module.exports = webpackMerge(baseConfig, {
+  devtool: '#source-map',
+  entry: {
+    'ngx-smartadmin': './src/index.ts'
+  },
+  output: {
+    path: util.root('lib'),
+    filename: '[name].js',
+    chunkFilename: '[id].js',
+    library: '[name]',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: [
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/core',
+    '@angular/http',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/router',
+    '@angular/forms',
+    '@angular/animations'
+  ],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+        LIB_VERSION: `'${require('../package.json').version}'`
+      }
+    }),
+    new UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { SmartAdminModule } from './smartadmin.module';
 
+let version = process.env.LIB_VERSION || 'dev';
+
 export {
-  SmartAdminModule
+  SmartAdminModule,
+  version
 };
