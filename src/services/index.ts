b82cc0b477fba64ac913dd3052c80990e5f90ed3
Jay.M.Hu <jay.m.hu@newegg.com>
2017-09-19 15:36:44 +0800

    service structure

---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import { Loading } from './loading/Loading';
+import { MessageBox } from './message-box/MessageBox';
+import { NotifyBox } from './notify-box/NotifyBox';
+
+export {
+  Loading,
+  MessageBox,
+  NotifyBox
+};
